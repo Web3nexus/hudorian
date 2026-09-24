4740a13a9c393cdec29f71029e097ba59f54fcb5
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-09-24 22:26:52 +0100

    Eliminate stale index.html and chunks: enable trailingSlash export and synchronize clean modal builds across root

--- _next/static/chunks/app/layout-a67904ad7b4cdfaf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1743:()=>{},7758:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1743,23)),Promise.resolve().then(n.bind(n,5007)),Promise.resolve().then(n.bind(n,6462))}},e=>{e.O(0,[334,372,441,794,358],()=>e(e.s=7758)),_N_E=e.O()}]);
